584317a5dc289be7bd432547516cc319b721b10f
Ryanads0908 <109008918+Ryanads0908@users.noreply.github.com>
2022-07-15 13:29:48 +0800

    second version

--- products.py
+++ products.py
@@ -4,13 +4,7 @@ while True:
 	if name == 'q':
 		break
 	price = input('請輸入商品價格：')
-	
-	p = []
-	p.append(name)
-	p.append(price)
-	# p = [name, price]
-	
-	products.append(p)
+	products.append([name, price])
 print(products)
 
 products[0][0]
